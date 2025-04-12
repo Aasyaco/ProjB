a695ff354b83e5d5fc9493fc96ecf69db1b34085
𝗚𝗘𝗢𝗥𝗚𝗘 <george.methq@gmail.com>
2025-04-12 10:21:57 +0600

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const apiRoutes = require('./api/index.js');
-
-const app = express();
-const PORT = process.env.PORT || 8080;
-
-app.use('/api', apiRoutes);
-
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
