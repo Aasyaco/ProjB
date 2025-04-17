d8ffe54603383923a9e2ae58bca885f3e7fe7ddf
𝗚𝗘𝗢𝗥𝗚𝗘 <george.methq@gmail.com>
2025-04-17 15:31:24 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const serverless = require("serverless-http");
-const express = require('express');
-const apiRoutes = require('./api/index.js');
+const express = require("express");
+const apiRoutes = require("./api/index.js");
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-app.use('/api', apiRoutes);
+// Use API routes
+app.use("/api", apiRoutes);
 
+// Start the server
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
